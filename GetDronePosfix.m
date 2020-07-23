52ccb21ab3ce0d3dbfe0cc2cde2ecb8faa71ee5d
Nile-Waldal <55334108+Nile-Waldal@users.noreply.github.com>
2020-07-23 11:41:23 -0600

    Update GetDronePosfix.m
    Previous for loop revision doesn't really work, instead of taking out name2, uses it when reading from timestamp file.

--- GetDronePosfix.m
+++ GetDronePosfix.m
@@ -4,31 +4,30 @@ function GetDronePos
 % Rinex position file
 name1='Rinex.txt';
 % Timestamp file
-%name2='Timestamp.txt';   --> variable not used
+name2='Timestamp.txt';
 % Images txt file
 name3='Images.txt';
-
 %% READ in position file
     format longE
     warning off;
     C0 = readtable(name1,'HeaderLines',4);
-    Rinex = zeros(size(C0,1),5);
+    Rinex=zeros(size(C0,1),5);
     for k=1:size(C0,1)
         % Year
-        Rinex(k,1)=str2double(C0{k,4});
+        Rinex(k,1)=C0{k,4};
         % UTM Northing
-        Rinex(k,2)=str2double(C0{k,30});
+        Rinex(k,2)=C0{k,30};
         % UTM Easting
-        Rinex(k,3)=str2double(C0{k,29});
+        Rinex(k,3)=C0{k,29};
         % Elevation, it should be in the CGVD2013 system
-        Rinex(k,4)=str2double(C0{k,38});
+        Rinex(k,4)=C0{k,38};
         % Time in hours since beginning of UTC day
         Rinex(k,5)= (Rinex(k,1)-fix(Rinex(k,1)))*24.0;
     end
+    %% READ in TimeStamp file
     
-%% READ in TimeStamp file
     C0 = readtable(name2);
-    TimeStamp = zeros(size(C0,1),10);
+    TimeStamp=zeros(size(C0,1),10);
     for i=1:size(C0,1)    
         % ID
         TimeStamp(i,1)=C0{i,1};
@@ -56,51 +55,41 @@ name3='Images.txt';
         TimeStamp(i,9)=0;
         TimeStamp(i,10)=TimeStamp(i,2)/60/60-fix(TimeStamp(i,2)/60/60/24)*24;
     end
-    
-    %% get Image names
+    %% get Images' name
     format longE
     C0 = readtable(name3,'ReadVariableNames',false);
 
-    %% getStampLocation - GPS continuous week count of 2055 starts on 2019-May-26 (Sunday) UTC
+    
+    %% getStampLocation
+    % GPS continuous week count of 2055 starts on 2019-May-26 (Sunday) UTC
     for j1=1:size(TimeStamp,1)-1
-        if abs(TimeStamp(j1,10)-TimeStamp(j1+1,10)) > 23
-            TimeStamp(j1+1:end,10) = TimeStamp(j1+1:end,10)+24;
         if abs(TimeStamp(j1,10)-TimeStamp(j1+1,10))>23
             TimeStamp(j1+1:end,10)=TimeStamp(j1+1:end,10)+24;
-        else
             break
         end
     end
     
     for j=1:size(Rinex,1)-1
         if abs(Rinex(j,5)-Rinex(j+1,5))>23
-        if abs(Rinex(j,5)-Rinex(j+1,5)) > 23
-            Rinex(j+1:end,5) = Rinex(j+1:end,5)+24;
-        else
+            Rinex(j+1:end,5)=Rinex(j+1:end,5)+24;
             break
         end
     end
     
     for j=1:size(TimeStamp,1)
-        Hour=TimeStamp(j,10);
-        for i=1:size(Rinex,1)-1
-            if (Rinex(i,5)-Hour)*(Rinex(i+1,5)-Hour) < 0
-                TimeStamp(j,7:9) = Rinex(i,2:4)+(Rinex(i+1,2:4)-Rinex(i,2:4))/(Rinex(i+1,5)-Rinex(i,5))*(Hour-Rinex(i,5))+[TimeStamp(j,4:5),-TimeStamp(j,6)]/1000;
-            elseif Rinex(i,5)-Hour == 0
-                TimeStamp(j,7:9) = Rinex(i,2:4)+[TimeStamp(j,4:5),-TimeStamp(j,6)]/1000;
-            else
-                break    
-                TimeStamp(j,7:9)=Rinex(i,2:4)+(Rinex(i+1,2:4)-Rinex(i,2:4))/(Rinex(i+1,5)-Rinex(i,5))*(Hour-Rinex(i,5))+[TimeStamp(j,4:5),-TimeStamp(j,6)]/1000;
-            elseif Rinex(i,5)-Hour == 0
-                TimeStamp(j,7:9)=Rinex(i,2:4)+[TimeStamp(j,4:5),-TimeStamp(j,6)]/1000;
-            else
-                break
-            end
-        end
+      Hour=TimeStamp(j,10);
+      for i=1:size(Rinex,1)-1
+          if (Rinex(i,5)-Hour)*(Rinex(i+1,5)-Hour)<0
+              TimeStamp(j,7:9)=Rinex(i,2:4)+(Rinex(i+1,2:4)-Rinex(i,2:4))/(Rinex(i+1,5)-Rinex(i,5))*(Hour-Rinex(i,5))+[TimeStamp(j,4:5),-TimeStamp(j,6)]/1000;
+              break
+          elseif  Rinex(i,5)-Hour==0
+               TimeStamp(j,7:9)=Rinex(i,2:4)+[TimeStamp(j,4:5),-TimeStamp(j,6)]/1000;
+               break
+          end
+      end
     end
     
     pix4d_data=C0;
-    
     for j=1:size(C0,1)
         Rows=C0{j,1};
         ImageNoChar=Rows{1};
@@ -110,6 +99,7 @@ name3='Images.txt';
         pix4d_data(j,3)={TimeStamp(d,7)};
         pix4d_data(j,4)={TimeStamp(d,9)};
     end
+% 
 name3=['UAV_camera_coords_' int2str(size(pix4d_data,1)) '.txt'];
     % ID Easting Northing Elevation   
 writetable(pix4d_data,name3,'WriteVariableNames',false);
