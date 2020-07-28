89ba1ce3c96e0a3faab026d2dbcb70a480e96664
Nile-Waldal <55334108+Nile-Waldal@users.noreply.github.com>
2020-07-28 11:12:10 -0600

    Update Rinex.c

--- Rinex.c
+++ Rinex.c
@@ -28,6 +28,34 @@ int main(void) {
   if(!file||!pfile){
     printf("Unable to open files.");
     return 1;
+  }
+  while(strstr(line,"END OF HEADER")==NULL){
+    fgets(line,256,file);
+    if(strstr(line,"TIME OF FIRST OBS")!=NULL){
+      fputs("                                                            0.2000 INTERVAL     \n",pfile);
+    }
+    fputs(line,pfile);
+    
+  }
+  while(i!=2){
+    fgets(line,256,file);
+    if(strstr(line,">")!=NULL){
+      i++;
+    }
+  }
+  do{
+    if(strstr(line,">")!=NULL){
+      ind=strstr(line,"000000 0");
+      for(i=strlen(line)-strlen(ind);i>=7;i--){
+        *(ind+i)=*(ind+i-1);
+      }
+    }
+    if(strstr(line,"END OF RINEX OBS DATA")!=NULL){
+      break;
+    }
+    fputs(line,pfile);
+  }
+  while(fgets(line,256,file));
   fclose(file);
   fclose(pfile);
   closedir(folder);
