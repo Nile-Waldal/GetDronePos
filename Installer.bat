d1349e66ffd20283fa43ea70bccaed2ad1bcafee
Nile-Waldal <55334108+Nile-Waldal@users.noreply.github.com>
2020-08-20 19:27:30 -0600

    Update Installer.bat

--- Installer.bat
+++ Installer.bat
@@ -1,5 +1,5 @@
 @ECHO off
 CD /D %~dp0
 SET location=%cd%
-IF NOT EXIST "C:\Program Files (x86)\Updating 3D Coordinates\" MD "C:\Program Files (x86)\Updating 3D Coordinates" "C:\Program Files (x86)\Updating 3D Coordinates\Files"
-IF NOT EXIST "C:\Program Files (x86)\Updating 3D Coordinates\Files\Updating 3D Drone Coordinates.bat" XCOPY /s "%location%\Files" "C:\Program Files (x86)\Updating 3D Coordinates\Files"
+IF NOT EXIST "%USERPROFILE%\Desktop\Updating 3D Coordinates" MD "%USERPROFILE%\Desktop\Updating 3D Coordinates" "%USERPROFILE%\Desktop\Updating 3D Coordinates\Files"
+IF NOT EXIST "%USERPROFILE%\Desktop\Updating 3D Coordinates\Files\Updating 3D Drone Coordinates.bat" XCOPY /s "%location%\Files" "%USERPROFILE%\Desktop\Updating 3D Coordinates\Files"
