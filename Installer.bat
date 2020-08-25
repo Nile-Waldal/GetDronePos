23ab3c03fdf78843b014167fca13a526aaa2732c
Nile-Waldal <55334108+Nile-Waldal@users.noreply.github.com>
2020-08-25 08:34:34 -0600

    Added changeable installation location

--- Installer.bat
+++ Installer.bat
@@ -4,6 +4,18 @@
 CD /D %~dp0
 SET location=%cd%
 
-::Moves files to Desktop\Updating 3D Coordinates if it hasn't yet been installed
-IF NOT EXIST "%USERPROFILE%\Desktop\Updating 3D Coordinates" MD "%USERPROFILE%\Desktop\Updating 3D Coordinates" "%USERPROFILE%\Desktop\Updating 3D Coordinates\Files" "%USERPROFILE%\Desktop\Updating 3D Coordinates\Output"
-IF NOT EXIST "%USERPROFILE%\Desktop\Updating 3D Coordinates\Files\Updating 3D Drone Coordinates.bat" XCOPY /s "%location%\Files" "%USERPROFILE%\Desktop\Updating 3D Coordinates\Files"
+::Opens File Browser
+set "psCommand="(new-object -COM 'Shell.Application')^
+.BrowseForFolder(0,'Please choose a installation folder.',0,0).self.path""
+
+::Sets folder chosen as install
+for /f "usebackq delims=" %%I in (`powershell %psCommand%`) do set "install=%%I"
+setlocal enableDelayedExpansion
+endlocal
+
+::Moves files to Updating 3D Coordinates if it hasn't yet been installed
+IF NOT EXIST "%install%\Updating 3D Coordinates" (
+MD "%install%\Updating 3D Coordinates" "%install%\Updating 3D Coordinates\Files" "%install%\Updating 3D Coordinates\Output"
+XCOPY /s "%location%\Files" "%install%\Updating 3D Coordinates\Files"
+MOVE "%install%\Updating 3D Coordinates\Files\Updating 3D Drone Coordinates.bat" "%install%\Updating 3D Coordinates"
+)
