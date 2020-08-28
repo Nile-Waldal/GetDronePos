ddb9bb956851fef895e7248622a3ea39cd2bd4c5
Nile-Waldal <55334108+Nile-Waldal@users.noreply.github.com>
2020-08-28 13:14:55 -0600

    Update Installer.bat

--- Installer.bat
+++ Installer.bat
@@ -4,14 +4,24 @@
 CD /D %~dp0
 SET location=%cd%
 
+:file
+
+ECHO Select an installation folder
+
 ::Opens File Browser
 set "psCommand="(new-object -COM 'Shell.Application')^
-.BrowseForFolder(0,'Please choose a installation folder.',0,0).self.path""
+.BrowseForFolder(0,'Please choose an installation folder.',0,0).self.path""
 
 ::Sets folder chosen as install
 for /f "usebackq delims=" %%I in (`powershell %psCommand%`) do set "install=%%I"
 setlocal enableDelayedExpansion
 endlocal
+
+IF NOT DEFINED "%install%" (
+GOTO :file
+)
+
+ECHO Installing...
 
 ::Moves files to Updating 3D Coordinates if it hasn't yet been installed
 IF NOT EXIST "%install%\Updating 3D Coordinates" (
@@ -19,3 +29,6 @@ MD "%install%\Updating 3D Coordinates" "%install%\Updating 3D Coordinates\Files"
 XCOPY /s "%location%\Files" "%install%\Updating 3D Coordinates\Files"
 MOVE "%install%\Updating 3D Coordinates\Files\Updating 3D Drone Coordinates.bat" "%install%\Updating 3D Coordinates"
 )
+
+ECHO Installed. Create PPP direct configuration before running program.
+PAUSE
